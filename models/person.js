fdd6545f9329c65a931bfd1f4fbbd25cb559a0d3
Shahid <shahid.sarker@gmail.com>
2020-04-24 15:22:28 -0400

    Person model

---
+++ models/person.js
@@ -0,0 +1,26 @@
+require("dotenv").config();
+const mongoose = require("mongoose");
+
+const url = process.env.MONGODB_URI;
+
+console.log("connecting to", url);
+
+mongoose
+  .connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((result) => console.log("connected to MongoDB"))
+  .catch((err) => console.log("error connecting to MongoDB", err.message));
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+});
+
+personSchema.set("toJSON", {
+  transform: (document, returnedObj) => {
+    returnedObj.id = returnedObj._id.toString();
+    delete returnedObj._id;
+    delete returnedObj.__v;
+  },
+});
+
+module.exports = mongoose.model("Person", personSchema);
