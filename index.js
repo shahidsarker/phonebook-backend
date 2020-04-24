8c5bcd45afc7019c7c46a828ffbb5b22aba515f6
Shahid <shahid.sarker@gmail.com>
2020-04-24 17:04:42 -0400

    delete from db

--- index.js
+++ index.js
@@ -70,8 +70,12 @@ app.get("/api/persons/:id", (req, res) => {
 
 app.delete("/api/persons/:id", (req, res) => {
   const id = Number(req.params.id);
-  persons = persons.filter((p) => p.id !== id);
-  res.status(204).end();
+
+  Person.findByIdAndRemove(req.params.id)
+    .then((result) => res.status(204).end())
+    .catch((error) => next(error));
+
+  // persons = persons.filter((p) => p.id !== id);
 });
 
 app.get("/api/persons", (req, res) => {
@@ -79,11 +83,6 @@ app.get("/api/persons", (req, res) => {
     res.json(persons.map((person) => person.toJSON()))
   );
 });
-
-generateId = () => {
-  const max = 1000;
-  return Math.floor(Math.random() * max);
-};
 
 app.post("/api/persons", (req, res) => {
   const body = req.body;
@@ -93,9 +92,6 @@ app.post("/api/persons", (req, res) => {
   if (!body.name) {
     return res.status(400).json({ error: "name is missing" });
   }
-  // if (persons.find((p) => p.name === body.name)) {
-  //   return res.status(409).json({ error: "name must be unique" });
-  // }
 
   const person = new Person({
     name: body.name,
